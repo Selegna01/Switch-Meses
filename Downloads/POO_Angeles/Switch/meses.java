5fc4ad81ffc3d75d13e84c9b1b05dcc82701466a
Selegna01 <20243d008@utez.edu.mx>
2025-05-13 09:42:36 -0600

    check del codigo

--- Downloads/POO_Angeles/Switch/meses.java
+++ Downloads/POO_Angeles/Switch/meses.java
@@ -15,52 +15,48 @@ public class meses{
                 nombreMes = "Enero";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 2:
                 nombreMes = "Febrero";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 3:
                 nombreMes = "Marzo";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 4:
                 nombreMes = "Abril";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 5:
                 nombreMes = "Mayo";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 6:
                 nombreMes = "Junio";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 7:
                 nombreMes = "Julio";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 8:
                 nombreMes = "Agosto";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 9:
                 nombreMes = "Septiembre";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 10:
                 nombreMes = "Enero";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 11:
                 nombreMes = "Octubre";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
-            case 1:
+            case 12:
                 nombreMes = "Noviembre";
-                System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
-                break;
-            case 1:
-                nombreMes = "Diciembre";
                 System.out.println("El mes correspondiente al número " + numeroMes + " es: " + nombreMes);
                 break;
                 default:;
