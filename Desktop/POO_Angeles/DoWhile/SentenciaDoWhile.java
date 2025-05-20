207b99f2c1ff73a04ab6a70607fdd2048206a3b0
Selegna01 <20243rd008@utez>
2025-05-20 09:29:01 -0600

    Codigo finalizado

--- Desktop/POO_Angeles/DoWhile/SentenciaDoWhile.java
+++ Desktop/POO_Angeles/DoWhile/SentenciaDoWhile.java
@@ -1,9 +1,22 @@
+//Author:Estrada Romero Angeles
+//Fecha:20/05/2025
+//Descripción:Sentencia DoWhile, con contador de 1 hasta que e usuario teclee "n".
+import java.util.Scanner;
 public class SentenciaDoWhile{
      public static void main(String[] args){
-        int contador = 10;//Inicialización
+       Scanner scanner = new Scanner (System.in);
+        int contador = 1;//Inicialización
+        char respuesta;
         do{
             System.out.println("Contador: " + contador);
             contador ++;//Incremento
-        }while(contador<=5);
+            System.out.println("Quieres continuar?(Escribe S para continuar, o N para detenerse) ");
+            respuesta = scanner.next().charAt(0);
+        }while(respuesta !='n');
+        System.out.println("Hasta pronto...");
      }
 }
+
+//JeigasCoins=55.95(1peso=595)
+//1)J a Pesos
+//2)De peso a Jeringa
