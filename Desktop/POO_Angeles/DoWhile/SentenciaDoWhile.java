c883c1a3b313b88dfe3c1c588e5c6c63034e39b0
Selegna01 <20243rd008@utez>
2025-05-20 08:52:45 -0600

    Codigo de tipo DoWhile

---
+++ Desktop/POO_Angeles/DoWhile/SentenciaDoWhile.java
@@ -0,0 +1,9 @@
+public class SentenciaDoWhile{
+     public static void main(String[] args){
+        int contador = 10;//Inicialización
+        do{
+            System.out.println("Contador: " + contador);
+            contador ++;//Incremento
+        }while(contador<=5);
+     }
+}
